89bdc344f5d81760bb03a2d754b12da4161ee516
giannisk81 <klapgit@outlook.com>
2015-03-15 14:56:56 +0000

    TipCalculator

---
+++ TipCalculator/TipCalculator/TipCalculator/MainPage.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using TipCalculator.Resources;
+
+namespace TipCalculator
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+
+            // Sample code to localize the ApplicationBar
+            //BuildLocalizedApplicationBar();
+        }
+
+        private void billAmountTextBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (billAmountTextBox.Text.Length > 0)
+            {
+                billAmountTextBox.Text = "£" + billAmountTextBox.Text;
+            }
+            else
+            {
+                billAmountTextBox.Text = "£0.00";
+            }
+        }
+
+        private void billAmountTextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            billAmountTextBox.Text = string.Empty;
+        }
+
+        private void billAmountTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+            performCalculation();
+        }
+
+        private void RadioButton_Click(object sender, RoutedEventArgs e)
+        {
+            performCalculation();
+        }
+
+        private void performCalculation()
+        {
+            var selection = myStackPanel.Children.OfType<RadioButton>().FirstOrDefault(r => r.IsChecked == true);
+            Tip.CalculateTip(billAmountTextBox.Text,double.Parse(selection.Tag.ToString()));
+            amountToTipBlock.Text = Tip.TipAmount;
+            totalTextBlock.Text = Tip.TotalAmount;
+
+        }
+
+     
+
+        // Sample code for building a localized ApplicationBar
+        //private void BuildLocalizedApplicationBar()
+        //{
+        //    // Set the page's ApplicationBar to a new instance of ApplicationBar.
+        //    ApplicationBar = new ApplicationBar();
+
+        //    // Create a new button and set the text value to the localized string from AppResources.
+        //    ApplicationBarIconButton appBarButton = new ApplicationBarIconButton(new Uri("/Assets/AppBar/appbar.add.rest.png", UriKind.Relative));
+        //    appBarButton.Text = AppResources.AppBarButtonText;
+        //    ApplicationBar.Buttons.Add(appBarButton);
+
+        //    // Create a new menu item with the localized string from AppResources.
+        //    ApplicationBarMenuItem appBarMenuItem = new ApplicationBarMenuItem(AppResources.AppBarMenuItemText);
+        //    ApplicationBar.MenuItems.Add(appBarMenuItem);
+        //}
+    }
+}
---
+++ TipCalculator/TipCalculator/TipCalculator/Tip.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TipCalculator
+{
+    public class Tip
+    {
+        public static string BillAmount {get; set;}
+        public static string TipAmount { get; set; }
+        public static string TotalAmount { get; set; }
+
+        public Tip()
+        {
+            BillAmount = string.Empty;
+            TipAmount = string.Empty;
+            TotalAmount = string.Empty;
+        }
+
+        public static void CalculateTip(string orAmount, double tipPr)
+        {
+            double billAmount = 0.0;
+            double tipAmount = 0.0;
+            double totalAmount = 0.0;
+
+            double.TryParse(orAmount.Replace("£", "").Trim(), out billAmount);
+            tipAmount = tipPr * billAmount;
+            totalAmount = tipAmount + billAmount;
+
+            BillAmount = string.Format("{0:C}", billAmount);
+            TipAmount = string.Format("{0:C}", tipAmount);
+            TotalAmount = string.Format("{0:C}", totalAmount);
+        }
+    }
+}
